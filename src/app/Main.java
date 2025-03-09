4796e2971c5100ce9c3ce49e854994421c294f9f
Nazar <nazar31032009@gmail.com>
2025-03-09 17:33:50 +0200

    Added function to convert degrees Fahrenheit to degrees Celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,4 +6,10 @@ public class Main {
         System.out.println("App for temperature converting.");
         System.out.println("Version 1.0.");
     }
+
+    // Фаренгейт в Цельсії
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
 }

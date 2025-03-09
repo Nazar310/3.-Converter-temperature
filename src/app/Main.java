c776fcee3c68486e289191fc141c4d157e12a80f
Nazar <nazar31032009@gmail.com>
2025-03-09 17:18:29 +0200

    Created Converter project with basic info output

---
+++ src/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Converter App.");
+        System.out.println("App for temperature converting.");
+        System.out.println("Version 1.0.");
+    }
+}
